c8231d0f858a750a681f34341aa185cd97d917a1
Jayanandmanik <98297791+Jayanandmanik@users.noreply.github.com>
2022-06-30 00:29:14 +0530

    def rev(n):     rem=0     while(n>0):         res=n%10         rem=rem*10+res         n=n//10     return rem def rrev(n):     n=-n     rem=0     while(n>0):         res=n%10         rem=rem*10+res         n=n//10     return-rem x=int(input()) if x>0:     print(rev(x)) else:     print(rrev(x))

---
+++ Reverse_Integer.py
@@ -0,0 +1,20 @@
+def rev(n):
+    rem=0
+    while(n>0):
+        res=n%10
+        rem=rem*10+res
+        n=n//10
+    return rem
+def rrev(n):
+    n=-n
+    rem=0
+    while(n>0):
+        res=n%10
+        rem=rem*10+res
+        n=n//10
+    return-rem
+x=int(input())
+if x>0:
+    print(rev(x))
+else:
+    print(rrev(x))
